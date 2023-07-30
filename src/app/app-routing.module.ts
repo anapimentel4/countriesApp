f9d97a504005edce565a3e84bb18a153d53806a8
Ana Pimentel <anapimentel004@gmail.com>
2023-07-30 13:23:51 -0400

    aplicacion de paises

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule, Component } from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactComponent } from './shared/pages/contact/contact.component';
+
+
+const routes: Routes=[
+
+
+{
+  path:'',
+  component: HomePageComponent
+},
+{
+  path: 'about',
+  component: AboutPageComponent
+},
+{
+  path: 'contact',
+  component: ContactComponent
+},
+{
+  path: '**',
+  redirectTo: ''
+},
+
+]
+
+@NgModule({
+imports:[
+  RouterModule.forRoot(routes)
+],
+exports:[
+  RouterModule
+]
+
+})
+export class AppRoutingModule { }
---
+++ src/app/shared/pages/contact/contact.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contact-contact',
+  templateUrl: './contact.component.html',
+  styles: [
+  ]
+})
+export class ContactComponent {
+
+}
